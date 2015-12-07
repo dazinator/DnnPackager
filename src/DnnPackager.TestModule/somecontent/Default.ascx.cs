0484646475c1e0a14c92ad37ed2b11df6130dd38
dazinator <darrell.tunnell@googlemail.com>
2015-12-07 00:11:39 +0000

    Changed projects to be .net 4, from .net 4.5.1 as .net 4 is generally more adopted.

---
+++ src/DnnPackager.TestModule/somecontent/Default.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DnnPackager.TestModule.somecontent
+{
+    public partial class Default : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/DnnPackager.Tests/TestPackageContent/Default.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnnPackager.TestModule.somecontent
+{
+    public partial class Default : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
