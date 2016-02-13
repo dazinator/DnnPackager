f8746d1cfcfa4d0fbe0c5f5a65c225195285aec0
dazinator <darrell.tunnell@googlemail.com>
2016-02-13 19:44:34 +0000

    #16

--- src/DnnPackager/content/test.txt.pp
+++ src/DnnPackager/content/test.txt.pp
@@ -1,2 +1,10 @@
 ﻿AbsoluteProjectDirectory = $AbsoluteProjectDirectory$
+OutputFileName = $OutputFileName$
+RootNamespace = $RootNamespace$
+FullPath = $FullPath$
+FileName = $FileName$
+AssemblyName = $AssemblyName$
+ApplicationIcon = $ApplicationIcon$
+RegisteredOrg = $registeredorganization$
+FriendlyModuleName = $friendlymodulename$
 
